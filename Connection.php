cfa67740ffafa5466a613f1acaab12129b6b5986
Andrey <xezzus@gmail.com>
2015-04-12 18:58:03 +0300

    fix

--- Connection.php
+++ Connection.php
@@ -3,8 +3,19 @@ namespace xezzus\yii\redis;
 
 class Connection {
 
-  public function test(){
-    return 'test';
+  public function shard($name='main'){
+
+    if(!isset($this->servers[$name]['connect'])){
+      if(!isset($this->servers[$name])) return false;
+      if(!isset($this->servers[$name]['host'])) $this->servers[$name]['host'] = '127.0.0.1';
+      if(!isset($this->servers[$name]['port'])) $this->servers[$name]['port'] = '6379';
+      $this->servers[$name]['connect'] = new \Redis();
+      $this->servers[$name]['connect']->connect($this->servers[$name]['host'],$this->servers[$name]['port']);
+      if(isset($this->servers[$name]['auth'])) $this->servers[$name]['connect']->auth($this->servers[$name]['auth']);
+    }
+
+    return $this->servers[$name]['connect'];
+
   }
 
 }
--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Yii2-redis
-connect to data base redis for Yii2 
-test
+This component use [Redis library](https://github.com/phpredis/phpredis)
+
+## Config
+
+```
+"components"=>[
+  'rd'=>[
+    'class'=>'\xezzus\yii\redis\Connection',
+    'servers'=>[
+      'main'=>['host'=>'127.0.0.1','port'=>'6379','auth'=>'1234567']
+    ]
+  ]
+]
+```
+
+# Example
+
+```
+Yii::$app->rd->shard('main')->set('test',microtime(1));
+
+var_dump(Yii::$app->rd->shard('main')->get('test'));
+```
